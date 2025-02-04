4be72e1d2c95244c2319312055f4a97b517ba378
Haiwei1027 <71439492+Haiwei1027@users.noreply.github.com>
2025-02-04 15:04:07 +0000

    initialise c++ project

---
+++ colony-sim.cpp
@@ -0,0 +1,12 @@
+﻿// colony-sim2.cpp : Defines the entry point for the application.
+//
+
+#include "colony-sim.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
